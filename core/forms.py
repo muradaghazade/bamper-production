f5ae8f73fc2b1386f45384c322dc5bf7e5b1c675
muradaghazade <xomurad41@gmail.com>
2024-03-04 20:44:03 +0400

    m

--- core/forms.py
+++ core/forms.py
@@ -19,25 +19,19 @@ class ManualForm(forms.ModelForm):
         fields = ['make', 'model', 'year' ,'title', 'image', 'phone']
 
 class ContactForm(forms.ModelForm):
+    name = forms.CharField(required = True, widget=forms.TextInput(attrs={'id': 'question', 'placeholder': 'Ad və soyad'}))
+    phone = forms.CharField(required = True, widget=forms.TextInput(attrs={'id': 'question', 'placeholder': 'Nömrə'}))
+    email = forms.CharField(required = True, widget=forms.EmailInput(attrs={'id': 'question', 'placeholder': 'Email'}))
+    message = forms.CharField(required = True, widget=forms.Textarea(attrs={'id': 'question', 'placeholder': 'Mesaj', 'class': 'message_inp', 'style': 'padding-top: 15px;'}))
     class Meta:
         model = Contact
         fields = '__all__'
-
-        widgets = {
-            'name': forms.TextInput(attrs={'id': 'question', 'placeholder': 'Ad və soyad'}),
-            'phone': forms.TextInput(attrs={'id': 'question', 'placeholder': 'Nömrə'}),
-            'email': forms.EmailInput(attrs={'id': 'question', 'placeholder': 'Email'}),
-            'message': forms.Textarea(attrs={'id': 'question', 'placeholder': 'Mesaj', 'class': 'message_inp', 'style': 'padding-top: 15px;'}),
-        }
 
 class IndexContactForm(forms.ModelForm):
+    name = forms.CharField(required = True, widget=forms.TextInput(attrs={'id': 'question', 'placeholder': 'Adınızı daxil edin', "class": "contact-name-input mt-4"}))
+    phone = forms.CharField(required = True, widget=forms.TextInput(attrs={'id': 'question', 'placeholder': 'Nömrənizi daxil edin', "class": "contact-name-input mt-3"}))
+    email = forms.CharField(required = True, widget=forms.EmailInput(attrs={'id': 'question', 'placeholder': 'E-poçt ünvanınızı daxil edin', 'class': 'contact-name-input mt-3'}))
+    message = forms.CharField(required = True, widget=forms.Textarea(attrs={'id': 'question', 'placeholder': 'Mesajınızı daxil edin', 'class': 'contact-name-textarea mt-3', 'style': 'padding-top: 15px;', "style": "resize: none;"}))
     class Meta:
         model = Contact
         fields = '__all__'
-
-        widgets = {
-            'name': forms.TextInput(attrs={'id': 'question', 'placeholder': 'Adınızı daxil edin', "class": "contact-name-input mt-4"}),
-            'phone': forms.TextInput(attrs={'id': 'question', 'placeholder': 'Nömrənizi daxil edin', "class": "contact-name-input mt-3"},),
-            'email': forms.EmailInput(attrs={'id': 'question', 'placeholder': 'E-poçt ünvanınızı daxil edin', 'class': 'contact-name-input mt-3'}),
-            'message': forms.Textarea(attrs={'id': 'question', 'placeholder': 'Mesajınızı daxil edin', 'class': 'contact-name-textarea mt-3', 'style': 'padding-top: 15px;', "style": "resize: none;"}),
-        }
